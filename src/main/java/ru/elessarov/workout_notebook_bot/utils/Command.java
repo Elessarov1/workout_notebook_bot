d0cdc4bceee882ecbb531046842539b5b9066306
Alexandr <Ale906206@yandex.ru>
2024-02-26 01:00:43 +0300

    feature: обработка неизвестного сообщения

--- src/main/java/ru/elessarov/workout_notebook_bot/utils/Command.java
+++ src/main/java/ru/elessarov/workout_notebook_bot/utils/Command.java
@@ -8,11 +8,12 @@ import java.util.Arrays;
 @AllArgsConstructor
 @Getter
 public enum Command {
-     START("/start", "стартовое приветствие", true),
-     HELP("/help", "выводит весь список команд"),
-     TRAININGS("/trainings", "выбрать одну из стандартных тренировок"),
-     MY_TRAININGS("/my_trainings", "показать сохраненные тренировки"),
-     SUBSCRIPTION("/subscription", "показать информацию о подписке");
+    START("/start", "стартовое приветствие", true),
+    HELP("/help", "выводит весь список команд"),
+    TRAININGS("/trainings", "выбрать одну из стандартных тренировок"),
+    MY_TRAININGS("/my_trainings", "показать сохраненные тренировки"),
+    SUBSCRIPTION("/subscription", "показать информацию о подписке"),
+    UNKNOWN("/unknown", "неизвестное сообщение", true);
 
      private final String name;
      private final String description;
@@ -28,6 +29,6 @@ public enum Command {
         return Arrays.stream(Command.values())
                 .filter(command -> command.getName().equals(text))
                 .findFirst()
-                .orElseThrow(() -> new RuntimeException("No such command " + text));
+                .orElse(UNKNOWN);
     }
 }
--- src/main/java/ru/elessarov/workout_notebook_bot/utils/Constants.java
+++ src/main/java/ru/elessarov/workout_notebook_bot/utils/Constants.java
@@ -5,5 +5,11 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
     public final String CANT_UNDERSTAND = "Извините, бот пока не умеет работать с этой командой";
-    public final String GREETING = "Привет";
+    public final String GREETING_TEXT = "Привет %s,\n" + "если нужна помощь с командами воспользуйся командой - %s";
+    public final String CHOOSE_TRAINING_TYPE = "Выбери вид тренировки";
+    public static final String HELP_BLOCK = """
+                        %s - %s\s
+                        %s - %s\s
+                        %s - %s\s
+                         %s - %s""";
 }
